a1086cc100b66129b6a95b0b12a33f5da1d0b686
Federico Rocha <federicorocha@MacBook-Pro-de-Federico.local>
2020-08-16 21:16:35 -0300

    Close #3 Header landing

---
+++ projects/wolox/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wlx-navbar',
+  template: 'navbar'
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
